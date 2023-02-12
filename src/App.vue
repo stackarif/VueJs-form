980d1325a7de485afe1e97e3f735732d1be8c458
stackarif <arifmahmudaj6@gmail.com>
2023-02-12 22:07:22 +0600

    Vuejs form

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <button @click="componentToRender='compHome'">Home</button>
+    <button @click="componentToRender='compContact'">Contact</button>
+    <button @click="componentToRender='compAbout'">About</button>
+
+    <keep-alive>
+      <component :is="componentToRender"></component>
+
+    </keep-alive>
+
+  </div>
+</template>
+
+<script>
+import compHome from './Components/Home'
+import compAbout from './Components/About'
+import compContact from './Components/Contact'
+
+export default{
+  data(){
+    return {
+      componentToRender: 'compContact'
+
+    }
+  },
+  components:{
+    compHome,
+    compAbout,
+    compContact
+
+  }
+
+}
+
+</script>
